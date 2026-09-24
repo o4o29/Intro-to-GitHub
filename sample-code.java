bb959207da31e05e39e146d66b21b8cd23cc40ca
Rose Ann Etang <roseannetang29@gmail.com>
2026-09-24 17:39:15 +0800

    Create sample-code.java

---
+++ sample-code.java
@@ -0,0 +1,6 @@
+public class SampleCode {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("This is my sample Java program.");
+    }
+}
